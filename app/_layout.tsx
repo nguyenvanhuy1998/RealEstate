b35e5af686060a35d3bfcc75ff20cb3ba4d45822
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-12-22 17:10:06 +0700

    Setup Fonts and Assets

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,26 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
 import "./global.css";
+import { useEffect } from "react";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
+    const [fontsLoaded, error] = useFonts({
+        "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+        "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+        "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+        "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+        "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+        "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    });
+    useEffect(() => {
+        if (fontsLoaded || error) {
+            SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded, error]);
+    if (!fontsLoaded && !error) {
+        return null;
+    }
     return <Stack />;
 }
