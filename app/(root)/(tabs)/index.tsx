95db8fe7557b88678c7a70e6cb4588718772c533
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2025-01-10 11:41:40 +0700

    Implement Profile UI

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,13 +10,9 @@ export default function Index() {
                 alignItems: "center",
             }}
         >
-            <Text className="font-rubik text-3xl color-primary-300  my-10">
+            <Text className="font-rubik text-3xl  my-10">
                 Welcome to ReState
             </Text>
-            <Link href={"/sign-in"}>Sign In</Link>
-            <Link href={"/explore"}>Explore</Link>
-            <Link href={"/profile"}>Profile</Link>
-            <Link href={"/properties/1"}>Property</Link>
         </View>
     );
 }
